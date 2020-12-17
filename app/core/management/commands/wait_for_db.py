61ea206c120296de9fafe262c092b1a965cfc66e
Hussam Alhajlah <hos200sa@gmail.com>
2020-12-17 10:28:21 +0300

    Configured postgres db.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,21 @@
+import time
+
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django Command to pause excution until database is availabel"""
+
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavailabel, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database availabel!'))
